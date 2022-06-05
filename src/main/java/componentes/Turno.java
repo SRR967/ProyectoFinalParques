550302a9359fa4e4d8d088a67ea151ba84ca843e
JhoanESG <94477658+JhoanESG@users.noreply.github.com>
2022-06-05 17:49:33 -0500

    Se agregaron el objeto jugador en estructuras, y la clase turno en componentes, cada uno con su recpectivo codigo

---
+++ src/main/java/componentes/Turno.java
@@ -0,0 +1,39 @@
+package componentes;
+
+import estructuras.Jugador;
+
+
+public class Turno {
+
+    private Jugador[] jugadores;
+    public Jugador[] getJugadores(){return jugadores;}
+
+    public void inicializar(int numJugadores){
+        contruirJugadores(numJugadores);
+    }
+
+    private void contruirJugadores(int numJugadores){
+        jugadores = new Jugador[numJugadores];
+        Jugador jugadorAnterior = null;
+
+        for(int i = 0; i < numJugadores; i++) {
+            var jugadorActual = new Jugador();
+            jugadorActual.numJugador = "" + i;
+
+            if(i != 0) {
+                // Si es el ultimo jugador
+                if(i == numJugadores - 1) {
+                    // Utilizo el primer jugador como jugador siguiente
+                    jugadorActual.siguiente = jugadores[0];
+                }
+                jugadorAnterior.siguiente = jugadorActual;
+                jugadorActual.anterior = jugadorAnterior;
+            }
+            jugadores[i] = jugadorActual;
+            jugadorAnterior = jugadorActual;
+        }
+        // Defino el jugador anterior del primer jugador
+        // El jugador anterior es el ultimo jugador
+        jugadores[0].anterior = jugadorAnterior;
+    }
+}
---
+++ src/main/java/estructuras/Jugador.java
@@ -0,0 +1,9 @@
+package estructuras;
+
+public class Jugador {
+    public String numJugador;
+    private String nombre;
+    private int numDeJugadas;
+    public Jugador siguiente;
+    public Jugador anterior;
+}
