32471b86b1aaa7785efa71c137134b369d5df36f
Santiago <santiramon967gmail.com>
2022-06-05 20:20:15 -0500

    Prueba

--- src/main/java/controladores/ControladorTablero.java
+++ src/main/java/controladores/ControladorTablero.java
@@ -4,6 +4,7 @@ import componentes.Tablero;
 import estructuras.Dado;
 import estructuras.Ficha;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -12,18 +13,30 @@ import java.awt.*;
 public class ControladorTablero {
 
     @FXML
-    private Button buttons;
+    private Button buttons []= new Button[68];
     private Tablero tablero;
     private Ficha ficha;
+    private AnchorPane anchorPane;
 
 
     @FXML
     public void inicializar() {
         tablero = new Tablero();
+        int cont=0;
+        for (Node node: anchorPane.getChildren()) {
+            if (cont==68){
+                break;
+            }else {
+                Button button =buttons[cont];
+                cont++;
+            }
+
+        }
 
 
 
     }
+
     @FXML
     private void clicCasillaHandler(MouseEvent event) {
         Button clickedButton = (Button) event.getSource();
